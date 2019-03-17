63cff2d01469f747303f7edaebe3807ef03819a6
15inb <40809571+15inb@users.noreply.github.com>
2019-03-16 20:44:43 -0400

    Add files via upload

---
+++ antispam.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const antispam = require('discord-anti-spam'); // Requiring this module.
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  // Module Configuration Constructor
+   antispam(client, {
+        warnBuffer: 4, // Maximum ammount of messages allowed to send in the interval time before getting warned.
+        maxBuffer: 6, // Maximum amount of messages allowed to send in the interval time before getting banned.
+        interval: 2000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting banned.
+        warningMessage: "If you continue spamming you will be banned.", // Message users receive when warned. (message starts with '@User, ' so you only need to input continue of it.)
+        muteMessage: "has been muted for spamming after being warned to stop.", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.)
+        maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned.
+        maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned.
+        deleteMessagesAfterBanForPastDays: 7, // Deletes the message history of the banned user in x days.
+        exemptRoles: ["Bot", "Owner", "Co-Owner", "Admin", "Moderator"], // Name of roles (case sensitive) that are exempt from spam filter.
+        exemptUsers: ["15inb#9872", "Sprickles#6403"] // The Discord tags of the users (e.g: MrAugu#9016) (case sensitive) that are exempt from spam filter.
+      });
+
+  // Rest of your code
+});
+
+client.on('message', msg => {
+  client.emit('checkMessage', msg); // This runs the filter on any message bot receives in any guilds.
+
+  // Rest of your code
+})
+
+client.login('NDk4MzI3NjIxNzk3NDEyODkw.DpsXMA.ELfl7_Sz24WOaZlCeVivd5c5ax4');
---
+++ index.js
@@ -0,0 +1,115 @@
+const Discord = require('discord.js'); //This can also be discord.js-commando or other node based packages!
+const dl = require('discord-leveling');
+
+//Create the bot client
+const client = new Discord.Client();
+
+//Whenever someone types a message this gets activated.
+//(If you use 'await' in your functions make sure you put async here)
+client.on('message', async message => {
+
+  //Set the prefix of the bot.
+  const settings = {
+    prefix: '!',
+  }
+
+  //This reads the first part of your message behind your prefix to see which command you want to use.
+  var command = message.content.toLowerCase().slice(settings.prefix.length).split(' ')[0];
+
+  //These are the arguments behind the commands.
+  var args = message.content.split(' ').slice(1);
+
+  //If the user that types a message is a bot account return.
+  if (message.author.bot) return;
+
+  //When someone sends a message add xp
+  var profile = await dl.Fetch(message.author.id)
+  dl.AddXp(message.author.id, 10)
+  //If user xp higher than 100 add level
+  if (profile.xp + 10 > 500) {
+    await dl.AddLevel(message.author.id, 1)
+    message.reply(`You just leveled up!! You are now level: ${profile.level + 1}`)
+  }
+
+  //If the message does not start with your prefix return.
+
+  if (command === 'profile') {
+
+    var user = message.mentions.users.first() || message.author
+
+    var output = await dl.Fetch(user.id)
+    message.channel.send(`Hey ${user.tag}! You have ${output.level} level(s)! and ${output.xp} xp!`);
+  }
+
+  if (command === 'setxp') {
+
+    if (!message.member.roles.some(r=>["Administrator", "Owner", "Head Administrator", "White Supremacy"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!");
+
+    var amount = args[0]
+    var user = message.mentions.users.first() || message.author
+
+    var output = await dl.SetXp(user.id, amount)
+    message.channel.send(`Hey ${user.tag}! You now have ${amount} xp!`);
+  }
+
+  if (command === 'setlevel') {
+
+    if (!message.member.roles.some(r=>["Administrator", "Owner", "Head Administrator", "White Supremacy"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!");
+
+    var amount = args[0]
+    var user = message.mentions.users.first() || message.author
+
+    var output = await dl.SetLevel(user.id, amount)
+    message.channel.send(`Hey ${user.tag}! You now have ${amount} levels!`);
+  }
+
+  if (command === 'leaderboard') {
+
+    //If you put a mention behind the command it searches for the mentioned user in database and tells the position.
+    if (message.mentions.users.first()) {
+
+      var output = await dl.Leaderboard({
+        search: message.mentions.users.first().id
+      })
+      message.channel.send(`The user ${message.mentions.users.first().tag} is number ${output.placement} on my leaderboard!`);
+
+      //Searches for the top 3 and outputs it to the user.
+    } else {
+
+      dl.Leaderboard({
+        limit: 3
+      }).then(async users => { //make sure it is async
+
+        var firstplace = await client.fetchUser(users[0].userid) //Searches for the user object in discord for first place
+        var secondplace = await client.fetchUser(users[1].userid) //Searches for the user object in discord for second place
+        var thirdplace = await client.fetchUser(users[2].userid) //Searches for the user object in discord for third place
+
+        message.channel.send(`My leaderboard:
+
+1 - ${firstplace.tag} : ${users[0].level} : ${users[0].xp}
+2 - ${secondplace.tag} : ${users[1].level} : ${users[1].xp}
+3 - ${thirdplace.tag} : ${users[2].level} : ${users[2].xp}`)
+
+      })
+
+    }
+  }
+
+  if (command == 'delete') {
+
+    var user = message.mentions.users.first()
+    if (!user) return message.reply('Pls, Specify a user I have to delete in my database!')
+
+  if (!message.member.roles.some(r=>["Administrator", "Owner", "Head Administrator", "White Supremacy"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!");
+
+    var output = await dl.Delete(user.id)
+    if (output.deleted == true) return message.reply('Succesfully deleted the user out of the database!')
+
+    message.reply('Error: Could not find the user in database.')
+
+  }
+
+})
+
+//Your secret token to log the bot in. (never show this to anyone!)
+client.login('NDk4MzI3NjIxNzk3NDEyODkw.DpsXMA.ELfl7_Sz24WOaZlCeVivd5c5ax4')
